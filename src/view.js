11bb23988d6aae7b6a27b14655f3afdc78494bcb
zheshek <zheny.bulechov@yandex.ru>
2026-01-30 09:21:56 +0300

    fix: render success feedback message

--- src/view.js
+++ src/view.js
@@ -55,15 +55,18 @@ const renderModal = (state, modalElement) => {
 };
 
 export default (elements, state) => (path, value) => {
-  const { input, modal, feedback } = elements;
+  const {
+    input,
+    modal,
+    feedback,
+  } = elements;
 
   if (path === 'form.status') {
     if (value === 'error') {
       input.classList.add('is-invalid');
-
-      feedback.textContent = i18next.t(state.form.error);
       feedback.classList.remove('text-success');
       feedback.classList.add('text-danger');
+      feedback.textContent = i18next.t(state.form.error);
     }
 
     if (value === 'valid') {
@@ -72,9 +75,9 @@ export default (elements, state) => (path, value) => {
       input.value = '';
       input.focus();
 
-      feedback.textContent = 'RSS успешно загружен';
       feedback.classList.remove('text-danger');
       feedback.classList.add('text-success');
+      feedback.textContent = i18next.t('RSS успешно загружен');
     }
 
     if (value === 'loading') {
